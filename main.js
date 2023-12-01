1891ea63b34876186c671e292fbb02b1f5ef127c
piojeet <manjhipiojeet@gmail.com>
2023-12-01 20:14:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,159 @@
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
+const nav_header = document.querySelector("nav .link");
+
+menuIcon.addEventListener("click", function () {
+    nav_header.classList.add("active");
+});
+
+closeIcon.addEventListener("click", function () {
+    nav_header.classList.remove("active");
+});
+
+
+
+
+
+
+
+function audioPlayer() {
+    const player = document.getElementById('player');
+    const progress = document.getElementById('progress');
+    const playBtn = document.getElementById('playIcon');
+    const bgColor = document.querySelector(".progress-bar");
+    const currentTimeDisplay = document.getElementById('current'); // Assuming 'current' is the element displaying the current time
+
+    playBtn.addEventListener('click', () => {
+        player.paused ? player.play() : player.pause();
+    });
+
+    player.onplay = () => {
+        playBtn.classList.remove('uil-play');
+        playBtn.classList.add('uil-pause');
+    };
+
+    player.onpause = () => {
+        playBtn.classList.add('uil-play');
+        playBtn.classList.remove('uil-pause');
+    };
+
+    player.ontimeupdate = () => {
+        const ct = player.currentTime;
+        const duration = player.duration;
+        const prog = Math.floor((ct * 100) / duration);
+
+        const minutes = Math.floor(ct / 60);
+        const seconds = Math.floor(ct % 60);
+        const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+        // Update time display if 'current' element exists
+        if (currentTimeDisplay) {
+            currentTimeDisplay.textContent = formattedTime;
+        }
+
+        // Update progress bar if 'progress' element exists
+        if (progress) {
+            progress.style.setProperty('--progress', `${prog}%`);
+
+            // Apply linear gradient background color to bgColor element
+            bgColor.style.background = `linear-gradient(to right, #00eaff ${prog}%, #072340 ${prog}%)`;
+        }
+    };
+
+    player.onended = () => {
+        // Reset bgColor background color when the audio/video ends
+        bgColor.style.background = '#072340';
+
+        // Reset time display to '0:00'
+        if (currentTimeDisplay) {
+            currentTimeDisplay.textContent = '0:00';
+        }
+    };
+
+}
+audioPlayer();
+
+
+
+
+
+
+
+
+
+// Remove the topelementsAdded variable
+window.addEventListener('load', function () {
+    addTopElementClass();
+});
+
+window.addEventListener('scroll', function () {
+    addTopElementClass();
+});
+
+function addTopElementClass() {
+    let elements = document.querySelectorAll('.top-up');
+    elements.forEach((element) => {
+        let elementTop = element.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (elementTop <= windowHeight && elementTop >= 0) {
+            element.classList.add('topelements');
+
+        } if (elementTop <= windowHeight * 0.90) {
+            element.classList.add('topelements');
+          }
+    });
+
+    let elementst = document.querySelectorAll('.left-up');
+    elementst.forEach((element) => {
+        let elementTop = element.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (elementTop <= windowHeight && elementTop >= 0) {
+            element.classList.add('topelements');
+
+        } if (elementTop <= windowHeight * 0.90) {
+            element.classList.add('leftelements');
+          }
+    });
+
+    let elementste = document.querySelectorAll('.right-up');
+    elementste.forEach((element) => {
+        let elementTop = element.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (elementTop <= windowHeight && elementTop >= 0) {
+            element.classList.add('topelements');
+
+        } if (elementTop <= windowHeight * 0.90) {
+            element.classList.add('rightelements');
+          }
+    });
+
+    let elementstf = document.querySelectorAll('.left-up-in');
+    elementstf.forEach((element) => {
+        let elementTop = element.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (elementTop <= windowHeight && elementTop >= 0) {
+            element.classList.add('topelements');
+
+        } if (elementTop <= windowHeight * 0.90) {
+            element.classList.add('leftelementsin');
+          }
+    });
+
+    let elementstey = document.querySelectorAll('.right-up-in');
+    elementstey.forEach((element) => {
+        let elementTop = element.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (elementTop <= windowHeight && elementTop >= 0) {
+            element.classList.add('topelements');
+
+        } if (elementTop <= windowHeight * 0.90) {
+            element.classList.add('rightelementsin');
+          }
+    });
+}
+
